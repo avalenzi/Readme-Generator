27ff2f0d65a25c743d5e7399cebddb06c8d64753
Anthony Valenzi <anthonymv1313@gmail.com>
2020-02-27 20:52:35 -0700

    updated index.js and api.js

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const questions = [
+// get data from other modules
+const mark = require("./utils/generateMarkdown.js");
+const questions = require("./utils/questions.js");
+const apiData = require("./utils/api.js");
+// get data from packages
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+// asynchronous function to gather all the data requested and condense into one data object
+(async function init() {
+    try {
+        const user = await inquirer.prompt(questions.questions);
+        const { data: githubData } = await apiData.getUser(user.githubName);
+        writeToFile({
+            ...user,
+            photo: githubData.avatar_url,
+            email: githubData.email || "User email is not public",
+            followers: githubData.followers
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
 
-function writeToFile(fileName, data) {
+// function to write markdown file with gathered data
+function writeToFile(data) {
+    const markdown = mark.generateMarkdown(data);
+    fs.writeFile("new-readme.md", markdown, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File generated");
+        }
+    });
 }
-
-function init() {
-
-}
-
-init();
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,20 @@
+
+// get packages
+require("dotenv").config();
+const axios = require("axios");
+
+// API call with axios to get GitHub data, using keys hidden in .env file
 const api = {
     getUser(username) {
-  
+        return axios
+            .get(
+                `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+            )
+            .catch(err => {
+                console.log("User not found", err);
+                process.exit(1);
+            });
     }
-  };
-  
-  module.exports = api;
+};
+
+module.exports = api;
